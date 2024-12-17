91a3b4af9284cb69970340df6e754515dc076c5d
hingorobilawal <hingorobilawal786@gmail.com>
2024-12-17 15:19:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+//chapter no 1 Alerts
+
+//Question no 1
+//alert("welcome")
+
+//Question no 2
+//alert("Erorr! please enter a valid code")
+
+//Question no 3
+//alert("welcome to js land /n happy coding")
+
+
+//Question no 4
+//alert("welcome to js land")
+//alert("prevent this page from creating additional diolgs")
+
+//Question no 5
+//alert("hello i can run js through my web browser's console")
+
+
+
+//chapter no 2 variables for strings
+
+
+//Question no 1
+//var name = ("bilal ahmed")
+//alert(name)
+
+
+//Question no 2
+//var messege = ("Hello World")
+//alert(message)
+
+
+//Question no 3
+//var biodata = (certified mobile application devolapment)
+//alert(biodata)
+
+
+//Question no 4
+//var foodname = ("pizza /n pizz /n piz /n pi /n p")
+//alert(foodname)
+
+//Question no 5
+//var email = ("hingorobilawal786@email.com")
+//alert(email)
+
+//Question no 6
+//var book = ("i am trying to learn from the book a smarter /n way to learn javascript")
+//alert(book)
+
+
+//var heading = ("<h1>yeah! I can write HTML content through javascript</h1>")
+//alert(heading)
+
+
+//chapter no 3
+
+
+
+//Question no 1
+//var yearold = ("i am 18 year old")
+//alert(yearold)
+
+
+//Question no 2
+//var visitors = ("you have visited 14 time this site")
+//alert(visitors)
+
+
+//Question no 3
+//var =birthyear "2004"
+//document.write("<h1>birth year</h1>")
+
+//Question no 4
+//var visitorname = ("bilal ordered /n")
+//var quantity = (5 /n)
+//var Product = ("T-Shirt on /n")
+//var App = ("online delivery App")
+
+
+//document.write(visitorname+quantity+)
